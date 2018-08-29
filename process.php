43786e13259461f1ee1767b0703eddd142dd05d1
tKikuchi <kikuchi@in32.com>
2018-08-29 17:11:06 +0900

    公開
    移住者抜き、協力隊関連のみ整備

--- process.php
+++ process.php
@@ -1,19 +1,46 @@
 <?php
+require_once "Mail.php";
 
-    $to = "";
-    $from = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
+// メールの文字セット
+define( "MAIL_CHARSET", "ISO-2022-JP" );
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"subject"} = "subject";
-    $fields{"message"} = "message";
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+// smtpサーバ接続情報
+$param = array(
+	"host" => "smtp.gmail.com"
+	, "port" => 587
+	, "auth" => true
+	, "username" => "ki910mo@gmail.com"
+	, "password" => "ki910mo3ki910mo3"
+);
 
-    $send = mail($to, $subject, $body, $headers);
+// 送信先情報
+$to = array( "info@setiju.com" );
+
+// 送信元＆件名＆本文を用意し、エンコード
+$from = $_REQUEST['name'];
+$from = mb_encode_mimeheader( $from, MAIL_CHARSET );
+$subject = $_REQUEST['subject'];
+$subject = mb_encode_mimeheader( $subject, MAIL_CHARSET );
+$body = mb_convert_encoding( $body, MAIL_CHARSET, "UTF-8" );
+
+// メールヘッダ
+$header = array(
+	"From" => $from
+	, "To" => implode( ",", $to )
+	, "Subject" => $subject
+	, "Content-Type" => "text/plain; charset=" . MAIL_CHARSET
+);
+
+// PEAR::Mailオブジェクト取得
+$obj =& Mail::factory( "smtp", $param );
+
+// メール送信
+$ret = $obj->send( $to, $header, $body );
+if ( PEAR::isError( $ret ) ) {
+	echo "Code[" . $ret->getCode() . "], Msg[" . $ret->getMessage() . "]\n";
+} else {
+	echo true;
+}
 
 ?>
